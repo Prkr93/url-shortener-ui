cfffe7bea4e294bb0ea9a9742a66c4d6344aeff7
Dillon Parker <prkr93@gmail.com>
2022-04-26 09:43:19 -0600

    Fix stupid form bug

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,11 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrls = (data) => {
+  return fetch('http://localhost:3001/api/v1/urls',{
+    method: 'POST',
+    headers: 'content-type:application/json',
+    body: JSON.stringify(data)
+  }).then(res => console.log(res))
+}
